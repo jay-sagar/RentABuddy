9c7d933d75e73b1ddfda486fa8490e97428254b4
Jay Sagar <67467477+jay-sagar@users.noreply.github.com>
2024-05-04 02:08:21 +0530

    Update Hero.jsx

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -22,15 +22,19 @@ function Hero() {
 
         <p className="mt-4 text-gray-600">
           Welcome to RentABuddy, your ultimate destination for finding the perfect companion for your next adventure or movie night in. With RentABuddy, you're never alone in creating memorable experiences.
-
-Whether you're craving the excitement of exploring new destinations or simply seeking the comfort of shared laughter over a movie, RentABuddy offers a diverse selection of trusted companions ready to join you on your journey.
-
-Browse our extensive roster of friendly buddies, each handpicked to ensure compatibility and reliability. From avid travelers to movie enthusiasts, our companions bring unique perspectives and vibrant personalities to every encounter.
-
-With RentABuddy, planning your next outing is effortless. Simply choose your desired companion, select your activity, and book with confidence knowing you're in good company. Whether it's a weekend getaway or a cozy night in, RentABuddy is your go-to destination for companionship on your terms.
-
-Join our community today and discover the joy of shared experiences with RentABuddy. Your perfect companion is just a booking away.
         </p>
+        <p className="mt-4 text-gray-600">
+          Whether you're craving the excitement of exploring new destinations or simply seeking the comfort of shared laughter over a movie, RentABuddy offers a diverse selection of trusted companions ready to join you on your journey.
+          </p>
+        <p className="mt-4 text-gray-600">
+          Browse our extensive roster of friendly buddies, each handpicked to ensure compatibility and reliability. From avid travelers to movie enthusiasts, our companions bring unique perspectives and vibrant personalities to every encounter.
+          </p>
+        <p className="mt-4 text-gray-600">
+With RentABuddy, planning your next outing is effortless. Simply choose your desired companion, select your activity, and book with confidence knowing you're in good company. Whether it's a weekend getaway or a cozy night in, RentABuddy is your go-to destination for companionship on your terms. 
+        </p>
+          <p className="mt-4 text-gray-600">
+          Join our community today and discover the joy of shared experiences with RentABuddy. Your perfect companion is just a booking away.
+          </p>
       </div>
     </div>
   </div>
