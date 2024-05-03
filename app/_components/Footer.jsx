70c80ff2da5edd71946683088e91127e6931497b
Jay Sagar <67467477+jay-sagar@users.noreply.github.com>
2024-05-04 02:02:28 +0530

    Update Footer.jsx

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -12,8 +12,7 @@ function Footer() {
     />
 
     <p className="mx-auto mt-6 max-w-md text-center leading-relaxed text-gray-500">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt consequuntur amet culpa cum
-      itaque neque.
+      Follow us on social media for the latest updates, exclusive offers, and inspiring companion stories:
     </p>
 
     <ul className="mt-12 flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-12">
