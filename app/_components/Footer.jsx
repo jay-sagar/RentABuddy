f6072a6584133c1e956c99ee83992653f1e78753
Jay Sagar <67467477+jay-sagar@users.noreply.github.com>
2024-05-04 02:03:07 +0530

    Update Footer.jsx

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -19,15 +19,6 @@ function Footer() {
       <li>
         <a className="text-gray-700 transition hover:text-gray-700/75" href="#"> About </a>
       </li>
-
-      <li>
-        <a className="text-gray-700 transition hover:text-gray-700/75" href="#"> Careers </a>
-      </li>
-
-      <li>
-        <a className="text-gray-700 transition hover:text-gray-700/75" href="#"> History </a>
-      </li>
-
       <li>
         <a className="text-gray-700 transition hover:text-gray-700/75" href="#"> Services </a>
       </li>
