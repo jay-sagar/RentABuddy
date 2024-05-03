fc4aba4bdc8410f0c17f76d75f19b28868cd0016
Jay Sagar <67467477+jay-sagar@users.noreply.github.com>
2024-05-04 01:59:12 +0530

    Update Header.jsx

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -38,7 +38,7 @@ function Header() {
   return (
     <div className="flex items-center justify-between p-4 shadow-sm">
       <div className="flex items-center gap-10">
-        <Image src="/logo.svg" alt="logo" width={180} height={80} />
+        <Image src="/logo.png" alt="logo" width={150} height={50} />
 
         <ul className="md:flex gap-8 hidden">
           {Menu.map((item, index) => (
