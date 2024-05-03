0431b58327c3024956aaa00c8198b3e28fa22825
Jay Sagar <67467477+jay-sagar@users.noreply.github.com>
2024-05-04 03:40:54 +0530

    Update CategoryList.jsx

--- app/(route)/search/_components/CategoryList.jsx
+++ app/(route)/search/_components/CategoryList.jsx
@@ -1,21 +1,3 @@
-"use client";
-import GlobalApi from "@/app/_utils/GlobalApi";
-import React, { useEffect, useState } from "react";
-import {
-  Command,
-  CommandDialog,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-  CommandSeparator,
-  CommandShortcut,
-} from "@/components/ui/command";
-import Image from "next/image";
-import { usePathname } from "next/navigation";
-import Link from "next/link";
-
 function CategoryList() {
   const [categoryList, setCategoryList] = useState([]);
   const params = usePathname();
@@ -32,24 +14,24 @@ function CategoryList() {
       setCategoryList(resp.data.data);
     });
   };
+
   return (
     <div className="h-screen mt-5 flex flex-col">
       <Command>
         <CommandInput placeholder="Type a command or search..." />
-         <CommandList className="overflow-visible">
+        <CommandList className="overflow-visible">
           <CommandEmpty>No results found.</CommandEmpty>
           <CommandGroup heading="Suggestions">
             {categoryList &&
               categoryList.map((item, index) => (
-                <CommandItem
-  key={index}
-  onClick={() => router.push(`/search/${item.attributes.Name}`)}
->
-  <div className={`p-2 flex gap-2 text-[14px] text-red-600 items-center rounded-md cursor-pointer w-full ${category === item.attributes.Name && 'bg-red-100'}`}>
-    <Image src={item.attributes?.Icon?.data.attributes?.url} alt="icon" width={25} height={25} />
-    <label>{item.attributes.Name}</label>
-  </div>
-</CommandItem>
+                <CommandItem key={index}>
+                  <Link href={'/search/' + item.attributes.Name}>
+                    <a className={`p-2 flex gap-2 text-[14px] text-red-600 items-center rounded-md cursor-pointer w-full ${category === item.attributes.Name && 'bg-red-100'}`}>
+                      <Image src={item.attributes?.Icon?.data.attributes?.url} alt="icon" width={25} height={25} />
+                      <label>{item.attributes.Name}</label>
+                    </a>
+                  </Link>
+                </CommandItem>
               ))}
           </CommandGroup>
         </CommandList>
